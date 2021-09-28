7907baa74a447b10be5b86c54e4af698768bbee1
kutay <kutaysahan07@gmail.com>
2021-09-28 13:25:09 +0300

    changed all.

---
+++ react/react-project1/src/app.js
@@ -0,0 +1,3 @@
+import { getData } from "./lib/service.js"; // service.js import edildi.
+
+getData(1); // userID degeri 1 olarak verildi.
---
+++ react/react-project1/src/lib/service.js
@@ -0,0 +1,7 @@
+import axios from 'axios'; // axios modulu import edildi.
+
+// getData async olarak tanimlandi. axios.get kullanilarak veriye ulasildi.
+export async function getData(userID){
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userID}`);
+    console.log(response.data);
+}
